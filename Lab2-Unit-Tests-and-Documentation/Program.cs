95d18457e736dd32c018b468b9e729c592c1db69
Sue Tarazi <suemachtley@gmail.com>
2020-03-18 10:46:39 -0700

    Add project files.

---
+++ Lab2-Unit-Tests-and-Documentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2_Unit_Tests_and_Documentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
